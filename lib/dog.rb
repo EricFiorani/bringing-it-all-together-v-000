a64e98e82af208adfdb5ca44af8eb593362a6016
Eric Fiorani <efiorani305@gmail.com>
2018-07-17 18:23:09 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -45,6 +45,38 @@ class Dog
     end
   end
 
+  def self.create(name:, breed:)
+    dog = Dog.new(name: name, breed: breed)
+    dog.save
+    dog
+  end
 
+  def self.find_by_id(id)
+    sql = "SELECT * FROM dogs WHERE id = ?"
+    result = DB[:conn].execute(sql, id)[0]
+    self.new(id: result[0],name: result[1], breed: result[2])
+  end
+
+  def self.find_or_create_by(name:, breed:)
+    dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?;", name, breed)
+    if dog.empty?
+      dog = self.create(name: name, breed: breed)
+    else
+      dog_data = dog[0]
+      dog = Dog.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
+    end
+    dog
+  end
+
+  def self.find_by_name(name)
+    sql = "SELECT * FROM dogs WHERE name = ? LIMIT 1"
+    row = DB[:conn].execute(sql, name).flatten
+    self.new(id: row[0], name: row[1], breed: row[2])
+  end
+
+  def update
+    sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.breed, self.id)
+  end
 
 end
